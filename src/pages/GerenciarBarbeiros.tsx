d3cdb32da96c19fb5b44c4279c43bedf35327a1c
[dyad] <9anjos@gmail.com>
2025-12-04 01:26:30 -0300

    [dyad] Corrigindo erros de tipo e importação em AuthContext e GerenciarBarbeiros. - wrote 2 file(s)

--- src/pages/GerenciarBarbeiros.tsx
+++ src/pages/GerenciarBarbeiros.tsx
@@ -7,6 +7,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { showSuccess, showError } from '@/utils/toast';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Link } from 'react-router-dom'; // Adicionado importação do Link
 import {
   AlertDialog,
   AlertDialogAction,
